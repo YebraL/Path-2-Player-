f7b21dc7fbdf8c91726c2ac60a24100cf809fd1c
YebraL <Product4conker@gmail.com>
2023-05-02 19:58:20 -0500

    Backend set up

---
+++ CharacterServer/CharacterServer_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CharacterserverAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CharacterServer_App'
---
+++ README.md
@@ -0,0 +1,6 @@
+# Starts 05/02/2023
+## Idea
+```
+I wanted to create a character creation helper for Pathfinder 2
+Start off create the Login
+```
