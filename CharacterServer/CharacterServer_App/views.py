a8748aea81389608ce5b5e7bf22d279d8670f43d
YebraL <Product4conker@gmail.com>
2023-05-02 21:17:49 -0500

    Scafold finished

--- CharacterServer/CharacterServer_App/views.py
+++ CharacterServer/CharacterServer_App/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def index(request):
+    index_file = open('static/index.html').read()
+    return HttpResponse(index_file)
